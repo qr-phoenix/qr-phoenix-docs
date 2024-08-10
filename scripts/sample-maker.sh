0d83c3d3e349caa7b318ceb0a970a7ca915dbbae
IggsGrey <jaygrey.jg@gmail.com>
2024-08-10 21:46:11 +0000

    Save sample maker script

---
+++ scripts/sample-maker.sh
@@ -0,0 +1,33 @@
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=round&color=0,0,0,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=square&image-url=https://logo.clearbit.com/twitter.com" -o twitter-black.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=dot&color=59,89,152,100&background-color=255,255,255,100&eye-style=square&external-eye-style=circle&image-url=https://logo.clearbit.com/facebook.com" -o facebook-blue.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=10,102,194,100&background-color=255,255,255,100&eye-style=circle&external-eye-style=rounded&image-url=https://logo.clearbit.com/linkedin.com" -o linkedin-blue.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=dot&color=255,0,0,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=pointy&image-url=https://logo.clearbit.com/youtube.com" -o youtube-red.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=round&color=30,215,96,100&background-color=255,255,255,100&eye-style=circle&external-eye-style=square&image-url=https://logo.clearbit.com/spotify.com" -o spotify-green.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=255,153,0,100&background-color=255,255,255,100&eye-style=square&external-eye-style=rounded&image-url=https://logo.clearbit.com/amazon.com" -o amazon-orange.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=207,16,32,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=pointy&image-url=https://logo.clearbit.com/tesla.com" -o tesla-red.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=37,211,102,100&background-color=255,255,255,100&eye-style=square&external-eye-style=circle&image-url=https://logo.clearbit.com/whatsapp.com" -o whatsapp-green.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=dot&color=0,120,215,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=pointy&image-url=https://i.ibb.co/cC6FK6M/favicon-1.png" -o expresspay.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=dot&color=255,205,0,100&background-color=0,0,0,100&eye-style=square&external-eye-style=square&image-url=https://logo.clearbit.com/mtn.com" -o mtn-yellow.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=dot&color=0,88,42,100&background-color=255,255,255,100&eye-style=circle&external-eye-style=pointy&image-url=https://logo.clearbit.com/ecobank.com" -o ecobank-green.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=229,0,0,100&background-color=255,255,255,100&eye-style=circle&external-eye-style=rounded&image-url=https://logo.clearbit.com/coca-cola.com" -o coca-cola-red.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=34,130,224,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=square&image-url=https://logo.clearbit.com/hubtel.com" -o hubtel-blue.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=round&color=0,107,183,100&background-color=255,255,255,100&eye-style=circle&external-eye-style=rounded&image-url=https://logo.clearbit.com/standardchartered.com" -o standard-chartered-blue.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=236,38,38,100&background-color=255,255,255,100&eye-style=circle&external-eye-style=circle&image-url=https://logo.clearbit.com/zenithbank.com" -o zenith-bank-red.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=round&color=0,176,240,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=pointy&image-url=https://logo.clearbit.com/paystack.com" -o paystack.png
+
+curl "/api/qr?data=https://rapidapi.com/jaygrey.jg/api/qr-phoenix&size=300&format=png&style=square&color=207,10,44,100&background-color=255,255,255,100&eye-style=rounded&external-eye-style=pointy&image-url=https://logo.clearbit.com/vodafone.com" -o telecel-red.png
