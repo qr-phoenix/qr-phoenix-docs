451b354065cddb94fa744c8ba0674100ea80f2dd
IggsGrey <jaygrey.jg@gmail.com>
2024-08-10 11:05:53 +0000

    update navbar contents

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,8 +79,13 @@ const config: Config = {
           position: 'left',
           label: 'API Docs',
         },
+        // {
+        //   label: "Playground",
+        //   href: "https://rapidapi.com/jaygrey.jg/api/qr-phoenix/playground",
+        //   position: "right",
+        // },
         {
-          label: "Playground",
+          html: "<span style='background: var(--ifm-code-background); text-transform:uppercase; color:var(--ifm-font-color-base); font-size:11px; padding:8px; border:0.1rem solid rgba(0, 0, 0, 0.1); border-radius:6px;'>Available on Rapid API<svg width='13.5' height='13.5' aria-hidden='true' viewBox='0 0 24 24' class='iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module'><path fill='currentColor' d='M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z'></path></svg></span>",
           href: "https://rapidapi.com/jaygrey.jg/api/qr-phoenix/playground",
           position: "right",
         }
